987ea6f339d0deec220ede429a78d69712c67b62
Jörg Henne <j.henne@levigo.de>
2016-01-14 16:38:20 +0100

    Improved support for CBLNumeric; CBLTable sized by length or count

--- src/main/java/org/jadice/recordmapper/cobol/CBLTable.java
+++ src/main/java/org/jadice/recordmapper/cobol/CBLTable.java
@@ -8,4 +8,6 @@ import java.lang.annotation.Target;
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CBLTable {
+  String sizeRef() default ""; 
+  String countRef() default ""; 
 }
