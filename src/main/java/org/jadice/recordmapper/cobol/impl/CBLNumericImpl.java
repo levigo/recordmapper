b1107162461cdcdf3601d6180ea4e24f93781d9c
Jörg Henne <j.henne@levigo.de>
2019-01-08 19:22:34 +0100

    Fix encoding and convert exception messages to en

--- src/main/java/org/jadice/recordmapper/cobol/impl/CBLNumericImpl.java
+++ src/main/java/org/jadice/recordmapper/cobol/impl/CBLNumericImpl.java
@@ -14,57 +14,59 @@ import org.jadice.recordmapper.impl.UnmarshalContext;
 public class CBLNumericImpl extends FieldMapping {
 	private CBLNumeric spec;
 
-	protected void init(Annotation a) {
+	protected void init(final Annotation a) {
 		this.spec = (CBLNumeric) a;
 	}
 
-	public int getSize(MappingContext ctx) {
+	public int getSize(final MappingContext ctx) {
 		return spec.value();
 	}
 
-	public void marshal(MarshalContext ctx, Object value) throws MappingException {
+	public void marshal(final MarshalContext ctx, final Object value) throws MappingException {
 		marshal(ctx, //
 				((Number) value).longValue(), //
 				ctx.getRecordAttributes(CBLRecordAttributes.class).getEndian(), //
 				spec.signed());
 	}
 
-	protected void marshal(MarshalContext ctx, long v, Endian endian,
-			boolean isSigned) throws MappingException {
+	protected void marshal(final MarshalContext ctx, final long v, final Endian endian,
+			final boolean isSigned) throws MappingException {
 		switch (getSize(ctx)){
 			case 1 :
 				if ((isSigned && (v > Byte.MAX_VALUE || v < Byte.MIN_VALUE))
 						|| (!isSigned && (v > 0xff || v < 0)))
-					throw new MappingException(
-							this, "Numerischer Wert für Feldlänge 1 außerhalb des zulässigen Bereiches");
+					throwValueOutOfRange(ctx);
 				ctx.put(isSigned ? (byte) v : (byte) (v & 0xff));
 				break;
 			case 2 :
 				if ((isSigned && (v > Short.MAX_VALUE || v < Short.MIN_VALUE))
 						|| (!isSigned && (v > 0xffff || v < 0)))
-					throw new MappingException(
-							this, "Numerischer Wert für Feldlänge 2 außerhalb des zulässigen Bereiches");
+					throwValueOutOfRange(ctx);
 				storeValue(ctx, endian, isSigned ? (short) v : (short) (v & 0xffff));
 				break;
 			case 4 :
 				if ((isSigned && (v > Integer.MAX_VALUE || v < Integer.MIN_VALUE))
 						|| (!isSigned && (v > 0xffffffffl || v < 0)))
-					throw new MappingException(
-							this, "Numerischer Wert für Feldlänge 4 außerhalb des zulässigen Bereiches");
+					throwValueOutOfRange(ctx);
 				storeValue(ctx, endian, isSigned ? (int) v : (int) (v & 0xffffffffl));
 				break;
 			case 8 :
 				if (!isSigned)
-					throw new MappingException(this, "Unsigned-Long-Werte nicht unterstützt");
+					throw new MappingException(this, "Unsigned long value not supported");
 				storeValue(ctx, endian, v);
 				break;
 			default :
-				throw new MappingException(this, "Ungültige Länge für CBLNumeric");
+				throw new MappingException(this, "Invalid length for a CBLNumeric");
 		}
 	}
 
-	private void storeValue(MarshalContext ctx, Endian endian,
-			long valueRespectingSign) throws MappingException {
+	private void throwValueOutOfRange(final MarshalContext ctx) throws MappingException {
+		throw new MappingException(
+				this, "Numeric value out of range for field length of " + getSize(ctx));
+	}
+
+	private void storeValue(final MarshalContext ctx, final Endian endian,
+			final long valueRespectingSign) throws MappingException {
 		int s = getSize(ctx);
 		byte buffer[] = new byte[s];
 
@@ -80,15 +82,15 @@ public class CBLNumericImpl extends FieldMapping {
 		ctx.put(buffer);
 	}
 
-	public Object unmarshal(UnmarshalContext ctx) throws MappingException {
+	public Object unmarshal(final UnmarshalContext ctx) throws MappingException {
 		return unmarshal(ctx,//
 				ctx.getBytes(getSize(ctx)), //
 				ctx.getRecordAttributes(CBLRecordAttributes.class).getEndian(), //
 				spec.signed());
 	}
 
-	protected Object unmarshal(UnmarshalContext ctx, final byte[] b,
-			Endian endian, boolean isSigned) throws MappingException {
+	protected Object unmarshal(final UnmarshalContext ctx, final byte[] b,
+			final Endian endian, final boolean isSigned) throws MappingException {
 		switch (getSize(ctx)){
 			case 1 :
 				long value = b[0];
@@ -102,12 +104,12 @@ public class CBLNumericImpl extends FieldMapping {
 			case 8 :
 				return extractValue(b, endian, ctx);
 			default :
-				throw new MappingException(this, "Ungültige Länge für CBLNumeric");
+				throw new MappingException(this, "Invalid length for a CBLNumeric");
 		}
 	}
 
-	private long extractValue(final byte[] byteArray, Endian endian,
-			MappingContext ctx) {
+	private long extractValue(final byte[] byteArray, final Endian endian,
+			final MappingContext ctx) {
 		long result = 0;
 		int shift = 0;
 		int s = getSize(ctx);
