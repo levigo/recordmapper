bbf468295a9fdd9d1cea3f812432cc3d5d68c3ab
Jörg Henne <Joerg.Henne@nuernberger.de>
2019-01-29 12:08:59 +0100

    Improve DateTime support with Java 8 classes

--- src/main/java/org/jadice/recordmapper/cobol/CBLDate.java
+++ src/main/java/org/jadice/recordmapper/cobol/CBLDate.java
@@ -4,13 +4,34 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.temporal.UnsupportedTemporalTypeException;
 
 @Target({
     ElementType.FIELD
 })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CBLDate {
+  public static final String DEFAULT_ZONE = "DEFAULT";
+
   public String value();
 
   public String nullDate() default "";
+
+  /**
+   * Specify the name of a {@link ZoneId} to use in conjunction with {@link Instant}s when
+   * formatting with formats which would otherwise throw a {@link UnsupportedTemporalTypeException}.
+   * Valid values are the ones accepted by {@link ZoneId#of(String)} as well as {@value #DEFAULT_ZONE}
+   * to use the {@link ZoneId#systemDefault()}.
+   */
+  public String zoneId() default "";
+
+  /**
+   * Specify the name of a {@link ZoneOffset} to use in conjunction with {@link Instant}s when
+   * formatting with formats which would otherwise throw a {@link UnsupportedTemporalTypeException}.
+   * Valid values are the ones accepted by {@link ZoneOffset#of(String)}.
+   */
+  public String zoneOffset() default "";
 }
--- src/main/java/org/jadice/recordmapper/cobol/impl/CBLDateImpl.java
+++ src/main/java/org/jadice/recordmapper/cobol/impl/CBLDateImpl.java
@@ -1,8 +1,19 @@
 package org.jadice.recordmapper.cobol.impl;
 
 import java.lang.annotation.Annotation;
-import java.text.DateFormat;
-import java.text.ParseException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.TemporalAccessor;
 import java.util.Date;
 
 import org.jadice.recordmapper.MappingException;
@@ -14,53 +25,108 @@ import org.jadice.recordmapper.impl.UnmarshalContext;
 
 public class CBLDateImpl extends FieldMapping {
   private CBLDate spec;
-  private DateFormat dateFormat;
+  private DateTimeFormatter dateFormat;
 
-  @Override
-  protected void init(Annotation a) {
-    spec = (CBLDate) a;
+  private ZoneId zoneId;
+
+  protected void init(Annotation a) throws MappingException {
+    this.spec = (CBLDate) a;
+
+    if (this.spec.zoneId().length() > 0 && this.spec.zoneOffset().length() > 0)
+      throw new MappingException("CBLDate allows either zoneId or zoneOffset, not both at " + this);
+
+    if ((this.spec.zoneId().length() > 0 || this.spec.zoneOffset().length() > 0)
+        && !(Instant.class.isAssignableFrom(field.getType())))
+      throw new MappingException(
+          "Can use zoneId or zoneOffset only in conjunction with a field of type Instant at " + this);
+
+    if (this.spec.zoneId().length() > 0)
+      zoneId = this.spec.zoneId().equals(CBLDate.DEFAULT_ZONE) ? ZoneId.systemDefault() : ZoneId.of(this.spec.zoneId());
+    if (this.spec.zoneOffset().length() > 0)
+      zoneId = ZoneOffset.of(this.spec.zoneOffset());
   }
 
-  @Override
   public int getSize(MappingContext ctx) {
     return spec.value().length();
   }
 
-  @Override
   public void marshal(MarshalContext ctx, Object value) throws MappingException {
-    final DateFormat df = getDateFormat();
+    DateTimeFormatter df = getDateFormat();
 
-    Date date = (Date) value;
-    if (null == date) {
+    if (null == value) {
       if (null == spec.nullDate())
         throw new MappingException(
             "The field " + field + " must not contain a null date unless its @CBLDate annotation specifies a nullDate");
       if (spec.nullDate().equals("NOW"))
-        date = new Date();
+        value = Instant.now();
       else {
         ctx.put(spec.nullDate());
         return;
       }
     }
 
-    ctx.put(df.format(date));
+    TemporalAccessor temp;
+    if (value instanceof Date)
+      temp = ((Date) value).toInstant();
+    else if (value instanceof TemporalAccessor)
+      temp = (TemporalAccessor) value;
+    else
+      throw new IllegalArgumentException("Don't know how to marshal a " + value.getClass());
+
+    if (temp instanceof Instant && null != zoneId) {
+      temp = ((Instant) temp).atZone(zoneId);
+    }
+
+    ctx.put(df.format(temp));
   }
 
-  @Override
   public Object unmarshal(UnmarshalContext ctx) throws MappingException {
-    final String s = ctx.getString(getSize(ctx));
-    final DateFormat df = getDateFormat();
+    String s = ctx.getString(getSize(ctx));
+    if (s.trim().isEmpty())
+      return null;
+
+    DateTimeFormatter df = getDateFormat();
 
     try {
-      return df.parse(s);
-    } catch (final ParseException e) {
+      Object value;
+      Class<?> type = field.getType();
+      if (type.isAssignableFrom(TemporalAccessor.class))
+        value = df.parse(s);
+      else if (type.isAssignableFrom(Instant.class)) {
+        value = df.parse(s, Instant::from);
+      } else if (type.isAssignableFrom(LocalDateTime.class))
+        value = df.parse(s, LocalDateTime::from);
+      else if (type.isAssignableFrom(LocalDate.class))
+        value = df.parse(s, LocalDate::from);
+      else if (type.isAssignableFrom(LocalTime.class))
+        value = df.parse(s, LocalTime::from);
+      else if (type.isAssignableFrom(OffsetDateTime.class))
+        value = df.parse(s, OffsetDateTime::from);
+      else if (type.isAssignableFrom(OffsetTime.class))
+        value = df.parse(s, OffsetTime::from);
+      else if (type.isAssignableFrom(ZonedDateTime.class))
+        value = df.parse(s, ZonedDateTime::from);
+      else if (type.isAssignableFrom(Date.class))
+        value = Date.from(df.parse(s, Instant::from));
+      else
+        throw new IllegalArgumentException("Don't know how to unmarshal into a " + type);
+
+      return value;
+    } catch (DateTimeParseException e) {
       throw new MappingException(this, e);
     }
   }
 
-  private DateFormat getDateFormat() {
-    if (null == dateFormat)
-      dateFormat = new SafeSimpleDateFormat(spec.value());
+  private DateTimeFormatter getDateFormat() {
+    if (null == dateFormat) {
+      DateTimeFormatterBuilder b = new DateTimeFormatterBuilder() //
+          .appendPattern(spec.value());
+
+      dateFormat = b.toFormatter();
+
+      if (null != zoneId)
+        dateFormat = dateFormat.withZone(zoneId);
+    }
     return dateFormat;
   }
 }
